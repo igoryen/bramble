ade80c5034acc44d26eb283e54e40500a148ff9c
igoryen <igor.yentaltsev@gmail.com>
2015-08-18 21:35:48 -0400

    Can 'create' an item

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -7,14 +7,44 @@ myapp.controller('productController', function ($scope) {
     {id: 'p04', name: 'Product 4', price: 4000, quantity: 23},
   ];
 
+  $scope.add = function () {
+    $scope.listProducts.push({
+      id: $scope.id, name: $scope.name, price: $scope.price, quantity: $scope.price
+    });
+    $scope.id = '';
+    $scope.name = '';
+    $scope.price = '';
+    $scope.quantity = '';
+  };
+
+  /**
+   * Update an item in list
+   * @param {type} id
+   * @returns {undefined}
+   */
+  $scope.selectEdit = function (id) {
+    var index = getSelectedIndex(id);
+    var product = $scope.listProducts[index];
+    $scope.id = product.id;
+    $scope.name = product.name;
+    $scope.price = product.price;
+    $scope.quantity = product.quantity;
+  }
+
+  /**
+   * Delete an item from list
+   * @param {type} id
+   * @returns {undefined}
+   */
   $scope.del = function (id) {
     var result = confirm('Are you sure?');
 
     if (result == true) {
       var index = getSelectedIndex(id);
+      // delete index from the array
       $scope.listProducts.splice(index, 1);
     }
-    // delete index from the array
+
 
   };
 
